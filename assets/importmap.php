ebff7df54b76ac57adca42e6901f00ee7255aa7f
Yassine Shili <yassine1.shili@outlook.fr>
2026-04-12 19:35:49 +0100

    fix: add missing importmap.php for AssetMapper

---
+++ assets/importmap.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'app' => [
+        'path' => './assets/app.js',
+        'entrypoint' => true,
+    ],
+    '@hotwired/stimulus' => [
+        'version' => '3.2.2',
+    ],
+    '@hotwired/turbo' => [
+        'version' => '7.3.0',
+    ],
+];
